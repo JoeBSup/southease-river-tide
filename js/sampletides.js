fe8f61c597cf27f0f975d7acc9b43d7991b84ef9
JoeBSup <joe@windseekerholidays.co.uk>
2026-07-16 20:02:12 +0100

    Add sample tide data for Newhaven
    This file contains sample tide data for Newhaven, including high and low tide times.

---
+++ js/sampletides.js
@@ -0,0 +1,42 @@
+/*
+==========================================================
+Southease River Tide v3.0
+sampleTides.js
+
+Sample Newhaven tide data.
+
+Replace with live API data later.
+==========================================================
+*/
+
+const SampleTides = {
+
+    location: "Newhaven",
+
+    date: new Date(),
+
+    tides: [
+
+        {
+            type: "High",
+            time: "03:18"
+        },
+
+        {
+            type: "Low",
+            time: "09:41"
+        },
+
+        {
+            type: "High",
+            time: "15:42"
+        },
+
+        {
+            type: "Low",
+            time: "22:08"
+        }
+
+    ]
+
+};
