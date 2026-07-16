d26b12661a18bad30b27e3a6322a31fe9d7a934e
JoeBSup <joe@windseekerholidays.co.uk>
2026-07-16 19:45:28 +0100

    Remove tide and river state display logic

--- js/app.js
+++ js/app.js
@@ -1,19 +1,5 @@
-window.onload = function () {
-
-    const tides = getTodaysTides();
-    const river = getRiverState();
-
-    document.getElementById("nextTide").innerHTML =
-        "<strong>Next High Water</strong><br>" +
-        tides.highWater;
-
-    document.getElementById("tides").innerHTML =
-        "▲ High Water " + tides.highWater +
-        "<br><br>" +
-        "▼ Low Water " + tides.lowWater;
-
-    document.getElementById("state").innerHTML =
-        "<strong>" + river.state + "</strong><br>" +
-        "Next change: " + river.nextChange;
-
-};
+river.riverState
+river.nextEvent
+river.nextTime
+river.southeaseHigh
+river.southeaseLow
