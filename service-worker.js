c370b3f3e334fecc6dc438f5d7561a55a25dc58a
JoeBSup <joe@windseekerholidays.co.uk>
2026-07-16 16:56:26 +0100

    Refactor service worker for caching and activation
    Updated cache name and added activation event to manage old caches.

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,45 @@
-const CACHE_NAME = "southease-river-v4";
+const CACHE_NAME = "southease-river-v2-2";
 
 const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./js/app.js",
-  "./js/countdown.js"
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./css/style.css",
+    "./js/app.js",
+    "./js/countdown.js"
 ];
 
+// Install
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+    );
 
-self.addEventListener(
-"install",
-event=>{
-
-event.waitUntil(
-caches.open(CACHE)
-.then(cache=>cache.addAll(files))
-);
-
+    self.skipWaiting();
 });
 
+// Activate
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames =>
+            Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            )
+        )
+    );
 
-self.addEventListener(
-"fetch",
-event=>{
+    self.clients.claim();
+});
 
-event.respondWith(
-caches.match(event.request)
-.then(response=>response || fetch(event.request))
-);
-
+// Fetch
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
 });
