9c6b666e500abbdae1cac8b0d9e4e50006cbf545
JoeBSup <joe@windseekerholidays.co.uk>
2026-07-16 20:06:39 +0100

    Refactor service worker for Southease River v3.0
    Updated service worker for Southease River v3.0 with new cache name and additional files.

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,149 @@
-const CACHE_NAME = "southease-river-v2-2";
+/*
+==========================================================
+Southease River Tide v3.0
+service-worker.js
+==========================================================
+*/
 
-const FILES_TO_CACHE = [
+const CACHE_NAME = "southease-river-v3-0";
+
+const APP_FILES = [
+
     "./",
     "./index.html",
+
     "./manifest.json",
+
     "./css/style.css",
+
+    "./js/sampleTides.js",
+    "./js/tideService.js",
+    "./js/riverCalculator.js",
+    "./js/countdown.js",
     "./js/app.js",
-    "./js/countdown.js"
+
+    "./icons/icon-192.png",
+    "./icons/icon-512.png"
+
 ];
 
-// Install
+
+/* -------------------------------------------------------
+   Install
+------------------------------------------------------- */
+
 self.addEventListener("install", event => {
+
+    console.log("Service Worker installing...");
+
     event.waitUntil(
+
         caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(FILES_TO_CACHE))
+
+            .then(cache => cache.addAll(APP_FILES))
+
     );
 
     self.skipWaiting();
+
 });
 
-// Activate
+
+/* -------------------------------------------------------
+   Activate
+------------------------------------------------------- */
+
 self.addEventListener("activate", event => {
+
+    console.log("Service Worker activated.");
+
     event.waitUntil(
-        caches.keys().then(cacheNames =>
-            Promise.all(
-                cacheNames.map(cache => {
-                    if (cache !== CACHE_NAME) {
-                        return caches.delete(cache);
-                    }
-                })
-            )
-        )
+
+        caches.keys()
+
+            .then(cacheNames => {
+
+                return Promise.all(
+
+                    cacheNames.map(cache => {
+
+                        if (cache !== CACHE_NAME) {
+
+                            console.log("Deleting old cache:", cache);
+
+                            return caches.delete(cache);
+
+                        }
+
+                    })
+
+                );
+
+            })
+
     );
 
     self.clients.claim();
+
 });
 
-// Fetch
+
+/* -------------------------------------------------------
+   Fetch
+------------------------------------------------------- */
+
 self.addEventListener("fetch", event => {
+
+    if (event.request.method !== "GET") {
+        return;
+    }
+
     event.respondWith(
+
         caches.match(event.request)
-            .then(response => response || fetch(event.request))
+
+            .then(cachedResponse => {
+
+                if (cachedResponse) {
+
+                    return cachedResponse;
+
+                }
+
+                return fetch(event.request)
+
+                    .then(networkResponse => {
+
+                        const responseClone =
+                            networkResponse.clone();
+
+                        caches.open(CACHE_NAME)
+
+                            .then(cache => {
+
+                                cache.put(
+                                    event.request,
+                                    responseClone
+                                );
+
+                            });
+
+                        return networkResponse;
+
+                    });
+
+            })
+
+            .catch(() => {
+
+                if (event.request.mode === "navigate") {
+
+                    return caches.match("./index.html");
+
+                }
+
+            })
+
     );
+
 });
