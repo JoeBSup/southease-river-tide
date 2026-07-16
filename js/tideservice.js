df85fe833557f7fc1225c26ab20f0574c5b66b19
JoeBSup <joe@windseekerholidays.co.uk>
2026-07-16 18:34:32 +0100

    Add tide service with sample tide data

---
+++ js/tideservice.js
@@ -0,0 +1,14 @@
+const sampleTides = [
+    {
+        type: "High",
+        time: "15:42"
+    },
+    {
+        type: "Low",
+        time: "22:08"
+    }
+];
+
+function getTodaysTides() {
+    return sampleTides;
+}
