6145225936d2896d7b96abbf95cc3bd6f3fd8def
JoeBSup <joe@windseekerholidays.co.uk>
2026-07-16 18:41:50 +0100

    Display sample tide data
    Display today's tides in the HTML elements for next tide and state.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,15 @@ window.onload = function () {
 
     const tides = getTodaysTides();
 
-    console.log(tides);
+    document.getElementById("nextTide").innerHTML =
+        "<strong>" + tides[0].type + "</strong><br>" + tides[0].time;
+
+    document.getElementById("tides").innerHTML =
+        "▲ " + tides[0].type + " " + tides[0].time +
+        "<br><br>" +
+        "▼ " + tides[1].type + " " + tides[1].time;
+
+    document.getElementById("state").textContent =
+        "Flood Tide";
 
 };
